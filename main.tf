6c3ab7130afdb5b741de0c38da356de7916f9e31
Mykyta Khomenko <mykyta.khomenko@gmail.com>
2026-01-17 15:13:41 +0100

    Little wipe-up

--- main.tf
+++ main.tf
@@ -2,22 +2,6 @@ module "kind_cluster" {
   source = "github.com/den-vasyliev/tf-kind-cluster"
 }
 
-# output "client_key" {
-#   value = kind_cluster.this.client_key
-# }
-
-# output "ca" {
-#   value = kind_cluster.this.cluster_ca_certificate
-# }
-
-# output "crt" {
-#   value = kind_cluster.this.client_certificate
-# }
-
-# output "endpoint" {
-#   value = kind_cluster.this.endpoint
-# }
-
 module "flux_bootstrap" {
   source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=kind_auth"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
@@ -28,14 +12,6 @@ module "flux_bootstrap" {
   config_crt        = module.kind_cluster.crt
   github_token      = var.GITHUB_TOKEN
 }
-
-# module "flux_bootstrap" {
-#   source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-#   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-#   private_key       = module.tls_private_key.private_key_pem
-#   config_path       = module.gke_cluster.kubeconfig
-#   github_token      = var.GITHUB_TOKEN
-# }
 
 module "github_repository" {
   source                   = "github.com/den-vasyliev/tf-github-repository"
@@ -52,10 +28,3 @@ module "tls_private_key" {
   ecdsa_curve = "P256"
 }
 
-# output "private_key_pem" {
-#   value = module.tls_private_key.private_key_pem
-# }
-
-# output "public_key_openssh" {
-#   value = module.tls_private_key.public_key_openssh
-# }
